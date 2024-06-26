162b3e82df4e53b511fbddffba29422818ffd27c
KauraniDivya <kauranidivya@gmail.com>
2024-06-26 16:30:12 +0530

    Products section updated and whatsapp integrated

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,58 +1,65 @@
-import React from "react"
-import logo from "./images/logo.png"
+import React from "react";
+import logo from "./images/logo.png";
 
-const Header = () =>{
-    return(
-        <>
-            <header class="header" data-header style={{position:"sticky"}}>
-    <div class="container">
+const Header = () => {
+  const phone = "+919016539191";
 
-      <a href="#home" class="logo flex justify-between gap-8">
-        <img src={logo} width="30" height="30" alt="Divya Enterprise logo" />
-        <p className="text-5xl mt-4">Divya Enterprise</p>
-      </a>
+  const handleWhatsAppClick = (phone, productName) => {
+    const message = `I am interested in your product: ${productName}`;
+    const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+    window.open(url, '_blank');
+  };
 
-      <nav class="navbar" data-navbar>
-
-        <div class="wrapper">
-          <a href="#home" class="logo">
+  return (
+    <header className="header" data-header style={{ position: "sticky" }}>
+      <div className="container">
+        <a href="#home" className="logo flex justify-between gap-8">
           <img src={logo} width="30" height="30" alt="Divya Enterprise logo" />
           <p className="text-5xl mt-4">Divya Enterprise</p>
-          </a>
+        </a>
 
-          <button class="nav-close-btn" aria-label="close menu" data-nav-toggler>
-            <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
-          </button>
-        </div>
+        <nav className="navbar" data-navbar>
+          <div className="wrapper">
+            <a href="#home" className="logo">
+              <img src={logo} width="30" height="30" alt="Divya Enterprise logo" />
+              <p className="text-5xl mt-4">Divya Enterprise</p>
+            </a>
 
-        <ul class="navbar-list">
+            <button className="nav-close-btn" aria-label="close menu" data-nav-toggler>
+              <ion-icon name="close-outline" aria-hidden="true"></ion-icon>
+            </button>
+          </div>
 
-          <li class="navbar-item">
-            <a href="#home" class="navbar-link" data-nav-link>Home</a>
-          </li>
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <a href="#home" className="navbar-link" data-nav-link>
+                Home
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#about" className="navbar-link" data-nav-link>
+                About
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#products" className="navbar-link" data-nav-link>
+                Products
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#choose" className="navbar-link" data-nav-link>
+                Why Choose Us?
+              </a>
+            </li>
+            <li className="navbar-item">
+              <a href="#visit" className="navbar-link" data-nav-link>
+                Visit Us
+              </a>
+            </li>
+          </ul>
+        </nav>
 
-          <li class="navbar-item">
-            <a href="#about" class="navbar-link" data-nav-link>About</a>
-          </li>
-
-          <li class="navbar-item">
-            <a href="#products" class="navbar-link" data-nav-link>Products</a>
-          </li>
-
-          <li class="navbar-item">
-            <a href="#choose" class="navbar-link" data-nav-link>Why Choose Us?</a>
-          </li>
-
-          <li class="navbar-item">
-            <a href="#visit" class="navbar-link" data-nav-link>Visit Us</a>
-          </li>
-
-        </ul>
-
-      </nav>
-
-      <div class="header-actions">
-
+        <div className="header-actions">
         <a href="tel:+919016539191" class="topbar-item link bg-yellow-100 p-5 pr-6 rounded-xl text-2xl text-black" style={{display: "flex"}}>
           <ion-icon name="call-outline" aria-hidden="true" class=" w-10 h-8 lg:hidden"></ion-icon>
           <div class="icon lg:flex hidden">
@@ -61,24 +68,24 @@ const Header = () =>{
           </div>          
         </a>
 
-        <a href="#" class="btn has-before">
-          <span class="span">Whatsapp Us</span>
+          <a
+            href="#"
+            className="btn has-before"
+            onClick={() => handleWhatsAppClick(phone, "Your Product Name")}
+          >
+            <span className="span">Whatsapp Us</span>
+            <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+          </a>
 
-          <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-        </a>
+          <button className="header-action-btn" aria-label="open menu" data-nav-toggler>
+            <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
+          </button>
+        </div>
 
-        <button class="header-action-btn" aria-label="open menu" data-nav-toggler>
-          <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
-        </button>
-
+        <div className="overlay" data-nav-toggler data-overlay></div>
       </div>
+    </header>
+  );
+};
 
-      <div class="overlay" data-nav-toggler data-overlay></div>
-
-    </div>
-  </header>
-        </>
-    )
-}
-
-export default Header
+export default Header;
