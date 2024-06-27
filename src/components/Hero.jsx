f87c7434180d217d19bb345d6c735d923d48c47b
KauraniDivya <kauranidivya@gmail.com>
2024-06-27 13:53:17 +0530

    responsiveness updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,44 +2,35 @@ import React from "react"
 import home from "./images/home.png"
 import shape2 from "./images/hero-shape-2.png"
 
-const Hero = () =>{
-    return(
+const Hero = () => {
+    return (
         <>
-              <section class="section hero has-bg-image" id="home" aria-label="home"
-        >
-        <div class="container">
+            <section className="section hero has-bg-image max-h-screen" id="home" aria-label="home">
+                <div className="container">
+                    <div className="hero-content">
+                        <h1 className="h1 lg:text-8xl text-6xl section-title text-center">
+                            <span className="span">Divya</span> Enterprise
+                        </h1>
 
-          <div class="hero-content">
+                        <p className="hero-text lg:text-4xl text-2xl">
+                            Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit.
+                        </p>
 
-            <h1 class="h1 lg:text-8xl text-6xl section-title text-center">
-              <span class="span">Divya</span> Enterprise
-            </h1>
+                        <a href="#courses" className="btn has-before">
+                            <span className="span">Find Products</span>
+                            <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
+                        </a>
+                    </div>
 
-            <p class="hero-text lg:text-4xl text-2xl">
-              Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit.
-            </p>
-
-            <a href="#courses" class="btn has-before">
-              <span class="span">Find Products</span>
-
-              <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-            </a>
-
-          </div>
-
-          <figure class="hero-banner">
-
-            <div class="img-holder one flex items-center justify-center lg:max-w-3xl max-w-xl lg:flex">
-              <img src={home} width="270" height="300" alt="hero banner" class="img-cover" />
-            </div>
-
-           
-            <img src={shape2} width="622" height="551" alt="" class="shape hero-shape-2" />
-
-          </figure>
-
-        </div>
-      </section>
+                    <figure className="hero-banner flex justify-center lg:justify-start">
+                        <div className="img-holder one flex justify-center items-center lg:max-w-4xl max-w-xl">
+                            <img src={home} width="270" height="300" alt="hero banner" className="img-cover" />
+                        </div>
+                        
+                        <img src={shape2} width="622" height="551" alt="" className="shape hero-shape-2 lg:mt-8 lg:max-w-4xl max-w-3xl" />
+                    </figure>
+                </div>
+            </section>
         </>
     )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -14,14 +14,14 @@ const DEHeader = () => {
     <header className="header" data-header style={{ position: "sticky" }}>
       <div className="container">
         <a href="#home" className="logo flex justify-between gap-8">
-          <img src={logo} width="30" height="30" alt="Divya Enterprise logo" />
-          <p className="text-5xl mt-4">Divya Enterprise</p>
+          <img src={logo} className="w-xl h-20" alt="Divya Enterprise logo" />
+          <p className="text-4xl mt-6">Divya Enterprise</p>
         </a>
 
         <nav className="navbar" data-navbar>
           <div className="wrapper">
             <a href="#home" className="logo">
-              <img src={logo} width="30" height="30" alt="Divya Enterprise logo" />
+              <img src={logo} className="w-xl h-20" alt="Divya Enterprise logo" />
               <p className="text-5xl mt-4">Divya Enterprise</p>
             </a>
 
@@ -30,7 +30,7 @@ const DEHeader = () => {
             </button>
           </div>
 
-          <ul className="navbar-list">
+          <ul className="navbar-list text-2xl">
             <li className="navbar-item">
               <a href="#home" className="navbar-link" data-nav-link>
                 Home
@@ -60,22 +60,22 @@ const DEHeader = () => {
         </nav>
 
         <div className="header-actions">
-        <a href="tel:+919016539191" class="topbar-item link bg-yellow-100 p-5 pr-6 rounded-xl text-2xl text-black" style={{display: "flex"}}>
+        <a href="tel:+919016539191" class="topbar-item link bg-yellow-100 p-5 pr-6 rounded-xl text-xl text-black font-medium" style={{display: "flex"}}>
           <ion-icon name="call-outline" aria-hidden="true" class=" w-10 h-8 lg:hidden"></ion-icon>
           <div class="icon lg:flex hidden">
-            <ion-icon name="call-outline" aria-hidden="true" class="mr-2 w-10 h-8 mt-1"></ion-icon>
+            <ion-icon name="call-outline" aria-hidden="true" class="mr-2 w-8 h-8 font-bold"></ion-icon>
             <span class="span">+91 9016539191</span>
           </div>          
         </a>
 
-          <a
-            href="#"
-            className="btn has-before"
+          <div
+            
+            className="btn has-before cursor-pointer p-2"
             onClick={() => handleWhatsAppClick(phone, "Your Product Name")}
           >
             <span className="span">Whatsapp Us</span>
             <ion-icon name="arrow-forward-outline" aria-hidden="true"></ion-icon>
-          </a>
+          </div>
 
           <button className="header-action-btn" aria-label="open menu" data-nav-toggler>
             <ion-icon name="menu-outline" aria-hidden="true"></ion-icon>
